15f0957fd1ef1eef4f11a3fef3ce81297111ee84
amac81 <arnaldocanelas@gmail.com>
2023-09-28 11:20:36 +0100

    BussinessException implemented

---
+++ src/exception/BussinessException.java
@@ -0,0 +1,11 @@
+package exception;
+
+public class BussinessException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BussinessException(String msg) {
+		super(msg);
+	}
+
+}

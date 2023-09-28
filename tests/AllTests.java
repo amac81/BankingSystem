e30f974d6a2440b21527d26ed9d44fd37d2b4706
amac81 <arnaldocanelas@gmail.com>
2023-09-28 11:29:35 +0100

    AllTests implemented

---
+++ tests/AllTests.java
@@ -0,0 +1,31 @@
+package business;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	ClientManagerTestEx1.class, 
+	ClientManagerTestEx2.class,
+	ClientManagerTestEx3.class, 
+	ClientManagerTestEx4.class, 
+	ClientManagerTestEx5.class,
+	ClientManagerTestEx6.class,
+	ClientManagerTestEx7.class, 
+	ClientManagerTestEx8.class,
+	AccountManagerTestEx1.class, 
+	AccountManagerTestEx2.class, 
+	AccountManagerTestEx3.class,
+	AccountManagerTestEx4.class })
+
+public class AllTests {
+
+}
+
+
+
+
+
+
+
+
